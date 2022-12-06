9b29fe8be6e51d84acb77055b30ebd00c2005f1c
Arti-pc <ma-l.lezhenko@yandex.ru>
2022-12-07 00:11:25 +0300

    done lesson abstractClass

--- oop/absractClass.php
+++ oop/absractClass.php
@@ -1,6 +1,7 @@
 <?php
 abstract class AbstractClass{
     abstract public function getValue();
+
     public function printValue()
     {
         echo 'Значение: ' . $this->getValue();
@@ -16,3 +17,55 @@ class classA extends AbstractClass{
         return $this -> value;
     }
 }
+abstract class HumanAbstract
+{
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    abstract public function getGreetings(): string;
+    abstract public function getMyNameIs(): string;
+
+    public function introduceYourself(): string
+    {
+        return $this->getGreetings() . '! ' .
+            $this->getMyNameIs() . ' ' . $this->getName() . '.';
+    }
+}
+
+class RussianHuman extends HumanAbstract {
+    public function getGreetings(): string
+    {
+        return 'Привет';
+    }
+    public function getMyNameIs() : string{
+        return 'Меня зовут';
+    }
+}
+
+class EnglishHuman extends HumanAbstract{
+    public function getGreetings(): string
+    {
+        return 'Hello';
+    }
+    public function getMyNameIs() : string{
+        return 'My name is';
+    }
+}
+//$object = new classA(45);
+//$object -> printValue();
+
+$humaR = new RussianHuman('Иван');
+
+$humaE = new EnglishHuman('Ivan');
+
+echo $humaR->introduceYourself().PHP_EOL;
+echo $humaE->introduceYourself();
